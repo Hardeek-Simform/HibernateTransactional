44dac8d3e631a54fe6e8085551c923952ee1e0dd
hardeek-simform <hardeek.b@simformsolutions.com>
2023-07-11 17:39:32 +0530

    Hibernate:Practical3:practical done

--- src/main/java/com/simform/supplychain/controller/Practical3Controller.java
+++ src/main/java/com/simform/supplychain/controller/Practical3Controller.java
@@ -1,45 +1,49 @@
 package com.simform.supplychain.controller;
 
 import com.simform.supplychain.entity.Suppliers;
+import com.simform.supplychain.repository.SupplierRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
 @RequestMapping("/practical")
 public class Practical3Controller {
     @Autowired
     private SupplierController supplierController;
 
+    @Autowired
+    private SupplierRepository supplierRepository;
+
     @GetMapping("/fail")
     public ResponseEntity<List<Suppliers>> failWithoutTransactional() {
         // here consider we will be adding multiple suppliers and that would fail in between
         Suppliers s1 = new Suppliers();
-        s1.setSupplierName("rakesh");
+        s1.setSupplierName("Failed but added");
         s1.setSid(10001);
         s1.setContactNo("90090909");
-        Suppliers s2 = new Suppliers();
-        s2.setSupplierName("rakesh1");
-        s2.setSid(10001);               // here the error will be generated
-        s2.setContactNo("8899898");
+        supplierRepository.save(s1);
+        Suppliers s2 = null;
+        supplierRepository.save(s2);
         return supplierController.getAllSuppliers();
     }
 
     @Transactional
-    @GetMapping("/true")
+    @GetMapping("/pass")
     public ResponseEntity<List<Suppliers>> passWithTransactional() {
         // here consider we will be adding multiple suppliers and that would fail in between
         Suppliers s1 = new Suppliers();
-        s1.setSupplierName("rakesh");
+        s1.setSupplierName("Passed person");
         s1.setSid(10001);
         s1.setContactNo("90090909");
-        Suppliers s2 = new Suppliers();
-        s2.setSupplierName("rakesh1");
-        s2.setSid(10001);               // here the error will be generated
-        s2.setContactNo("8899898");
+        supplierRepository.save(s1);
+        Suppliers s2 = null;
+        supplierRepository.save(s2);
         return supplierController.getAllSuppliers();
     }
 
